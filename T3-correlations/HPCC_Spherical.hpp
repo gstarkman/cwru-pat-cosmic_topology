19edfecd51d40af91d977c1c21ddb09bcec64256
joshosborne <jco35@case.edu>
2017-11-15 18:13:04 -0800

    Add files via upload

---
+++ T3-correlations/HPCC_Spherical.hpp
@@ -0,0 +1,13 @@
+#ifndef HPCC_SPHERICAL_HPP_INCLUDED
+#define HPCC_SPHERICAL_HPP_INCLUDED
+
+#include <complex>
+#include <stdlib.h>
+#include <vector>
+#include <gsl_specfunc.h>
+
+/// these are the typical mathematical functions for expansion in spherical harmonics
+double j_l(int l, double x);
+std::complex<double> Y_lm(int l, int m, double polar, double azimuth);
+
+#endif // HPCC_SPHERICAL_HPP_INCLUDED
